13be72ce2c18614c1c2c1b862625eee9c62df06e
Diego Iturrieta Gajardo <110798454+SanDBIG@users.noreply.github.com>
2024-02-18 19:46:52 -0300

    avance
    avance

---
+++ 03.Strings/MetodosCaracter.py
@@ -0,0 +1,13 @@
+cadena = 'Estoy utilizando los metodos de Python'
+cadena2 = "ESTOY UTILIZANDO LOS METODOS DE PYTHON"
+
+print(cadena.lower()) #lower se utiliza para transformar todo a miniscula
+print(cadena2.lower())
+
+print(cadena.upper()) #upper transforma todo a mayuscula
+
+print(cadena.capitalize()) #capitalize transforma la primera letra en mayuscula
+
+print(cadena.title()) #cada letra que inicia una palabra queda en mayuscula
+
+print(cadena.swapcase()) #cada letra pasa a la inversa de su tamaño
