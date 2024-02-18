8a5034831119f4194c455120dc4a2779bcce0401
Diego Iturrieta Gajardo <110798454+SanDBIG@users.noreply.github.com>
2024-02-18 18:12:44 -0300

    avance
    avance

---
+++ 03.Strings/DebanadoStrings.py
@@ -0,0 +1,6 @@
+cadena = "Este es un ejemplo de Substring (Debanado de Cadenas)"
+
+print(cadena[0 : 10])
+print(cadena[0 : 11])
+print(cadena[20 : ]) #empieza desde atras
+print(cadena[-1]) #empieza desde atras
