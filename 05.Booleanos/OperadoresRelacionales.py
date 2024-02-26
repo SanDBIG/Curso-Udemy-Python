3d3dac025e332ccabc524d039df0cc2d8b82ac8b
Diego Iturrieta Gajardo <110798454+SanDBIG@users.noreply.github.com>
2024-02-26 19:08:03 -0300

    avance booleanos
    avance

---
+++ 05.Booleanos/OperadoresRelacionales.py
@@ -0,0 +1,33 @@
+# Operadores relacionales
+# Hacen referencia a condiciones entre datos, tambien pueden ser
+# vistos como comparaciones
+
+# OPERADORES RELACIONALES
+# Operador == "igual que" EJ: 5 == 69 --> False
+# operador != "diferente de" EJ: 5!= 3 -->True
+# Operador <  "menor que" EJ: 6<20 -->False
+# Operador >  "Mayor que" 100>1 -->True
+# Operador <= "Menor o igual que" 90<=90 -->True
+# Operador >= "Mayor o igual que" 100>= 101 --> True
+
+# OPERADORES LOGICOS
+# Hacen referencia a condiciones entre datos, tambien pueden ser
+# vistos como comparaciones
+
+# Operador "and" descripcion "y" EJ: 10>1 and 70<=70 --> True
+# Operador "or" descripcion "o" EJ: 50>= 80 or 6 == 6 --> True
+# Operador "not" descripcion "no a" EJ: 3.14>3 --> True --> False
+
+# AND
+# Forma de trabajo:
+# True and True EJ: True
+# True and False EJ: False
+# False and True EJ: False
+# False and False EJ: False
+
+# OR
+# Forma de trabajo:
+# True or True EJ: True
+# True or False EJ: True
+# False or True EJ: True
+# False or False EJ: False
